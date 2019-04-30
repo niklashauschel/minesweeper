2e82dc24dfa67f36e57c1b30249022a98a19ff43
Fetzer <till.fetzer@hpe.com>
2019-04-30 22:29:24 +0200

    board is created

--- src/logic.py
+++ src/logic.py
@@ -1,77 +1,95 @@
-#Is onnly playing ground
+
 import random
 import numpy as np
 """
-# Here is your grid
+# Here is rowsour grid
 grid = np.random.binomial(1, 0.2, size=(3,3))
 
-# Request two random integers between 0 and 3 (exclusive)
+# Request two random integers between 0 and 3 (ecolumsclusive)
 indices =  np.random.randint(0, high=3, size=2)
 
-# Extract the row and column indices
+# Ecolumstract the row and column indices
 i = indices[0]
 j = indices[1]
 
 # Put 2 at the random position
 grid[i,j] = 10
 """
-class playfield():
-    """
-    This is the logical side off the playfield
-    try:
-    Unix methods approach: every method do one thing, put this good
 
+
+class board():
     """
-    def __init__(self, length, wide, bombs):
+    This is the logical side off the plarowsfield
+    trrows:
+    Unicolums methods approach: everrows method do one thing, put this good
+    """
+    def __init__(self, colums, rows, bombs):
         """
         input: length:number and wide.number
         do: create out of  input a  two dimensional list
         output: the two dimensional list
         """
-        notbombs=wide*length - bombs
-        self.board = [10]*bombs + notbombs*[0]
-        #random.shuffle(board)
-        self.board = np.array(board).reshape(length,wide)
-       
-        
+        self.colums = colums
+        self.rows = rows
+        self.bombs = bombs
+        notbombs = self.colums * self.rows - bombs
+        self.board = [10]*self.bombs + notbombs*[0]
+        random.shuffle(self.board)
+        self.board = np.array(self.board).reshape(colums, rows)
+        print(self.board)
 
-    def placebombs2darray(self, bombs):
+    def placebombs2darrarows(self, bombs):
         """
         input: number of bombs
-        do: put the bombs randomly in the playfield
+        do: put the bombs randomlrows in the plarowsfield
         output: 
         attention: do not put two bombs on the same field
 
 
         """
-        for i in range(1,bombs):
-            ranlegth=random.randint(0,self.length)
-            ranwide=random.randint(0,self.wide)
-            if self.playfield[ranlegth][ranwide] != 'x':
-                self.playfield[ranlegth][ranwide] = 'x'
-                neighbarfrombombs(ranlegth,ranwide)
+        for i in range(1, bombs):
+            ranlegth = random.randint(0, self.length)
+            ranwide = random.randint(0, self.wide)
+            if self.plarowsfield[ranlegth][ranwide] != 'colums':
+                self.plarowsfield[ranlegth][ranwide] = 'colums'
+                neighbarfrombombs(ranlegth, ranwide)
             else:
-                bombs+=1
+                bombs += 1
         
-    def placebombsmatrix(bombs):
+    def placebombsmatricolums(bombs):
+        pass
 
-
+    def createwarnfileds(self):
+        result = np.where(self.board == 10)
+        listOfCoordinates = list(zip(result[1], result[0]))
+        print(listOfCoordinates)
+        for cord in listOfCoordinates:   # funktioniert 
+            columsofbomb = cord[1]              
+            rowsofbomb = cord[0]
+            print('columsofbomb: ', columsofbomb)
+            print('rowsofomb: ', rowsofbomb)
+            for i in [-1, 0, 1]:            # marowsbe safe one loop  
+                for j in [-1, 0, 1]:
+                    columsneighbor = columsofbomb + i
+                    rowsneighbor = rowsofbomb + j
+                    if(columsneighbor >= 0 and 
+                        columsneighbor < self.colums and 
+                        rowsneighbor >= 0 and rowsneighbor < self.rows and
+                            not(i == 0 and j == 0)):
+                            if(self.board[columsneighbor][rowsneighbor] != 10):
+                                print('neighbarcolums:', columsofbomb + i)
+                                print('neighabrrows: ', rowsofbomb + j)
+                                print(i, j)
+                                
+                                self.board[columsneighbor][rowsneighbor] += 1
+                    else:
+                        print('elseneighbarcolums:', columsofbomb + i)
+                        print('elseneighabrrows: ', rowsofbomb + j)
+                        print(i, j)
                 
+        print(self.board)                        
 
+objplarowsfield = board(5, 8, 9)
+objplarowsfield.createwarnfileds()
 
-
-
-
-        pass
-    def createwarnfileds(self, length, wide):
-        result = 
-
-       
-
-    
-        
-        pass
-
-playfield(5,8,3)
-#createwarnfileds()
-
+print(objplarowsfield)
