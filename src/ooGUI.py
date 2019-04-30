ab4c7f34bc37cce1daa7b6c05937fe1865fc2f55
niklaaaas98 <niklas.hauschel@web.de>
2019-04-30 18:42:46 +0200

    Updated button image resizeable

--- src/ooGUI.py
+++ src/ooGUI.py
@@ -2,14 +2,15 @@ from tkinter import *
 
 # zwei Buttons: Einer um die Anwendung zu verlassen (QUIT) und der andere mit dem Text "Hello" startet die Methode write_slogan, die dann entsprechenden Text ausgibt
 
+
 class Configuration:
     def __init__(self, master):
         self.master = master
         self.v = IntVar()
 
-        # self.frame = Frame(master)
+        # frame = Frame(master)
 
-        # self.frame.grid()
+        # frame.grid()
 
         self.label1 = Label(self.master, text='Enter your Name')
         self.textfield = Text(self.master, width=15, height=3)
@@ -19,7 +20,6 @@ class Configuration:
 
         self.button = Button(self.master, text="QUIT", fg ='red', bg = 'black', command=self.destroyMenu)
         self.buttonNewWindow = Button(self.master, text="New", fg ='red', bg = 'black', command=self.goto)
-
 
         self.label1.grid(row=0, column=0, columnspan=2, sticky="N")
         self.textfield.grid(row=1, column=0, rowspan=1, columnspan=2, sticky="NESW")
@@ -38,17 +38,20 @@ class Configuration:
         degree_of_difficulty = 1
         root2 = Toplevel(self.master)
         myGUI = Game(root2, player, degree_of_difficulty)
+        Grid.rowconfigure(root2, 0, weight=1)
+        Grid.columnconfigure(root2, 0, weight=1)
         root2.title("Minesweeper Game")
-        root2.grid_rowconfigure(0, weight=1)
-        root2.grid_columnconfigure(0, weight=1)
         root2.update()
         root2.minsize(root2.winfo_width(), root2.winfo_height())
         root2.geometry('400x400')
+        root2.mainloop()
+
 
 def endGame():
     print('TODO Endgame')
 
-def handleButtonClickRight(event):
+
+def handleButtonClickLeft(event):
     if event.widget['text'] == 'x':
         print('You clicked on a mine')
         endGame()
@@ -60,31 +63,25 @@ def handleButtonClickRight(event):
     event.widget.unbind('<Button-3>')
 
     
-
-def handleButtonClickLeft(event):
+def handleButtonClickRight(event):
     if event.widget['bg'] == 'blue':
-        event.widget.config(bg='white')
+        pass
+        #event.widget.config(bg='white')
     else:
-        event.widget.config(bg='blue')
+        pass
+        #event.widget.config(bg='blue')
     print("hi left", event.widget['text'])
-
+    print(event.widget.grid_info())
 
 
 class Game:
      
     def __init__(self, master, player, degree_of_difficulty):
+          # source: https://stackoverflow.com/questions/7591294/how-to-create-a-self-resizing-grid-of-buttons-in-tkinter
+
         self.master = master
         self.player = player
-        self.frame = Frame(master)
-
-        rangex = 0
-        rangey = 0
-        if degree_of_difficulty == 1:
-            rangex = 10
-            rangey = 10
-
-        Grid.rowconfigure(self.master, 0, weight=1)
-        Grid.columnconfigure(self.master, 0, weight=1)
+        self.frame = Frame(self.master)
         self.frame.grid(row=0, column=0, sticky=N+S+E+W)
         self.grid = Frame(self.frame)
         self.grid.grid(sticky=N+S+E+W, column=0, row=0)
@@ -96,34 +93,31 @@ class Game:
         self.name = Label(self.frame, text=self.player)
         self.time.grid(row=0, column=0, sticky="NW", columnspan=10)
         self.label1.grid(row=0, column=0, sticky="N", columnspan=10)
-        self.name.grid(row=0, column=00, sticky="NE", columnspan=10)
+        self.name.grid(row=0, column=0, sticky="NE", columnspan=10)
 
-        self.photo = PhotoImage(file="imagen.png")
-        #self.photo2 = self.photo.subsample(10, 10)
-        
-        # source: https://stackoverflow.com/questions/7591294/how-to-create-a-self-resizing-grid-of-buttons-in-tkinter
-        
+        self.photo = PhotoImage(file="test.png")
 
-        for x in range(rangex):
-            for y in range(1, rangey):
+        self.rangex = 0
+        self.rangey = 0
+        if degree_of_difficulty == 1:
+            self.rangex = 10
+            self.rangey = 10
+        for x in range(self.rangex):
+            for y in range(1, self.rangey):
+
                 name = str(x) + " " + str(y)
-                if(x==0 and y==1):
-                    #btn = Button(self.frame, text='x', width="1",height="1")
-                    btn = Button(self.frame, text='x', image=self.photo)
-                else:
-                    #btn = Button(self.frame, text=name, width="1",height="1")
-                     btn = Button(self.frame, text=name, image=self.photo)
+                self.btn = Button(self.frame, image=self.photo, text=name)
+                self.btn.grid(column=x, row=y, sticky=N+S+E+W)
+                self.btn.bind("<Button-1>", handleButtonClickLeft)
+                self.btn.bind("<Button-3>", handleButtonClickRight)
 
-                btn.grid(column=x, row=y, sticky=N+S+E+W)
-                btn.bind("<Button-1>", handleButtonClickRight)
-                btn.bind("<Button-3>", handleButtonClickLeft)
                 Grid.columnconfigure(self.frame, x, weight=1)
                 Grid.rowconfigure(self.frame, y, weight=1)
-   
 
     def destroyMenu(self):
         self.master.destroy()
 
+    
 def main():
     root = Tk()
     config = Configuration(root)
