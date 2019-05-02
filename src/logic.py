08ef97ee8fcff18caf0c0b5813bea7e50d80af85
Fetzer <till.fetzer@hpe.com>
2019-05-02 21:55:31 +0200

    first try except block

--- src/logic.py
+++ src/logic.py
@@ -1,42 +1,29 @@
 
 import random
 import numpy as np
-"""
-# Here is rowsour grid
-grid = np.random.binomial(1, 0.2, size=(3,3))
-
-# Request two random integers between 0 and 3 (ecolumsclusive)
-indices =  np.random.randint(0, high=3, size=2)
-
-# Ecolumstract the row and column indices
-i = indices[0]
-j = indices[1]
-
-# Put 2 at the random position
-grid[i,j] = 10
-"""
 
 
 class board():
     """
-    This is the logical side off the plarowsfield
-    trrows:
-    Unicolums methods approach: everrows method do one thing, put this good
+    
     """
-    def __init__(self, colums, rows, bombs):
+    def __init__(self, rows, colums, bombs):
         """
-        input: colums and rows and bombs all numbers 
-        do: create a list with bombs and notboms fileds and shuffle them randomly 
+        input: rows and colums and bombs all numbers
+        do: create a list with bombs and notboms fileds and shuffle them randomly
             then formate this list to an 2d array
         out: the 2d array with random bombs
         """
-        self.colums = colums
         self.rows = rows
+        self.colums = colums
         self.bombs = bombs
-        notbombs = self.colums * self.rows - bombs
-        self.board = [10]*self.bombs + notbombs*[0] # 10 is standing for bombs
-        random.shuffle(self.board)
-        self.board = np.array(self.board).reshape(colums, rows)
+        notbombs = self.rows * self.colums - bombs
+        if (notbombs <= 0):
+            self.board = [10]*self.bombs + notbombs*[0]  # 10 is standing for bombs
+            random.shuffle(self.board)
+            self.board = np.array(self.board).reshape(rows, colums)
+        else:
+            console.log("There are to many bombs")
 
     def getBoard(self):
         """
@@ -48,39 +35,37 @@ class board():
         """
         simple getter for special value on index
         """
-        return(self.board[colum][row])
-
+        try:
+            return(self.board[colum][row])
+        except IndexError:
+            console.log("This is a mistake in the GUI")
 
     def createWarnFields(self):
         """
         input: the board with only bombs and filed with no bombs
         do: write on the filed with no bombs how much bombs are int the near
         output: the filed with everyfiled the number of bombs in the near
-
         """
         result = np.where(self.board == 10)
         listOfCoordinates = list(zip(result[1], result[0]))
         print(listOfCoordinates)
-        for cord in listOfCoordinates:   # funktioniert 
-            columsofbomb = cord[1]              
-            rowsofbomb = cord[0]
-         
-            for i in [-1, 0, 1]:            # marowsbe safe one loop  
+        for cord in listOfCoordinates:   # funktioniert
+            rowsofbomb = cord[1]
+            columsofbomb = cord[0]
+            for i in [-1, 0, 1]:            # macolumsbe safe one loop
                 for j in [-1, 0, 1]:
-                    columsneighbor = columsofbomb + i
-                    rowsneighbor = rowsofbomb + j
-                    if(columsneighbor >= 0 and 
-                        columsneighbor < self.colums and 
-                        rowsneighbor >= 0 and rowsneighbor < self.rows and
+                    rowsneighbor = rowsofbomb + i
+                    columsneighbor = columsofbomb + j
+                    if(rowsneighbor >= 0 and
+                        rowsneighbor < self.rows and
+                        columsneighbor >= 0 and columsneighbor < self.colums and
                             not(i == 0 and j == 0)):
-                            if(self.board[columsneighbor][rowsneighbor] != 10):
-                               
-                                
-                                self.board[columsneighbor][rowsneighbor] += 1
-                   
+                            if(self.board[rowsneighbor][columsneighbor] != 10):
+                                self.board[rowsneighbor][columsneighbor] += 1             
 
-objplarowsfield = board(5, 8, 9)
-objplarowsfield.createWarnFields()
-value = objplarowsfield.getvaluefromBoard(2, 4)
-
+objplacolumsfield = board(1, 2, 0)
+objplacolumsfield.createWarnFields()
+value = objplacolumsfield.getvaluefromBoard(0, 0)
+board = objplacolumsfield.getBoard()
 print(value)
+print(board)
